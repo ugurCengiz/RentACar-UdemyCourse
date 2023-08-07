e8a40fbe7de2d8af29cc42954108280c637180de
uğur cengiz <ugur_nobre@hotmail.com>
2023-08-07 18:47:53 +0300

    Add project files.

---
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+
+namespace Application.Features.Brands.Commands.Create;
+
+public class CreateBrandCommand:IRequest<CreatedBrandResponse>
+
+{
+    public string Name { get; set; }
+
+    public class CreateBrandCommandHandler:IRequestHandler<CreateBrandCommand,CreatedBrandResponse>
+    {
+        public Task<CreatedBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            CreatedBrandResponse createdBrandResponse = new CreatedBrandResponse();
+            createdBrandResponse.Name = request.Name;
+            createdBrandResponse.Id = new Guid();
+            return null;
+
+        }
+    }
+
+}
---
+++ Application/Services/Repositories/IBrandRepository.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+
+namespace Application.Services.Repositories;
+
+public interface IBrandRepository: IAsyncRepository<Brand>,IRepository<Brand>
+{
+
+}
