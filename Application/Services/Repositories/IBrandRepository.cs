16c46c7f93d15ce0522c0092fd35797bfb195300
uğur cengiz <ugur_nobre@hotmail.com>
2023-08-08 16:26:17 +0300

    08.08.23 Persistence Layer

--- Application/Services/Repositories/IBrandRepository.cs
+++ Application/Services/Repositories/IBrandRepository.cs
@@ -1,8 +1,9 @@
-﻿using Domain.Entities;
+﻿using Core.Persistence.Repositories;
+using Domain.Entities;
 
 namespace Application.Services.Repositories;
 
-public interface IBrandRepository: IAsyncRepository<Brand>,IRepository<Brand>
+public interface IBrandRepository: IAsyncRepository<Brand,Guid>,IRepository<Brand,Guid>
 {
 
 }
---
+++ Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence.Contexts;
+
+public class BaseDbContext:DbContext
+{
+    protected IConfiguration Configuration { get; set; }
+
+    public DbSet<Brand> Brands { get; set; }
+
+
+    public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration):base(dbContextOptions)
+    {
+        Configuration = configuration;
+        Database.EnsureCreated();
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+    }
+}
